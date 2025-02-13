bc9a0c9f49a4f26c5c07e1b9e7a90fe323afb787
Yundongji <yundongji111@163.com>
2025-02-13 21:42:09 +0800

    Add files via upload

--- config_client.bat
+++ config_client.bat
@@ -1,10 +1,11 @@
 : DeekSeek To Go  v1.0.0  by 运动鸡
 : Please open this file using GB 2312 (ANSI) encoding.
 : 如果你不知道下列配置的含义，请勿编辑，请不保存并退出编辑器！
+: 非配置项中的内容可能包含了程序运行的部分代码，请勿随意编辑，以免造成未知错误！
 : 如果你要添加环境变量，请在指定处添加（行数不够可回车加行）。
 : 配置编辑完直接保存退出并重启整个程序即可。
 
-: 关闭控制台回显，不用动。
+: （非配置项）关闭控制台回显。
 @echo off
 
 : 在此处添加你需要的环境变量。
@@ -23,5 +24,8 @@ set OLLAMA_NOHISTORY=0
 : 启动聊天时为Ollama指定的参数，各个参数间用空格隔开。
 set ollama_chat_parameter=--verbose
 
-: 启动主页，不用动。
+: （非配置项）此配置文件的版本，升级DeepSeek To Go时，若发现新版的配置文件比该文件版本高，你应该将旧的配置迁移到新版配置文件，并将旧版本替换。
+set config_version_client=1.0
+
+: （非配置项）启动主页命令。
 home
--- config_server.bat
+++ config_server.bat
@@ -1,16 +1,17 @@
 : DeekSeek To Go  v1.0.0  by 运动鸡
 : Please open this file using GB 2312 (ANSI) encoding.
 : 如果你不知道下列配置的含义，请勿编辑，请不保存并退出编辑器！
+: 非配置项中的内容可能包含了程序运行的部分代码，请勿随意编辑，以免造成未知错误！
 : 如果你要添加环境变量，请在指定处添加（行数不够可回车加行）。
 : 配置编辑完直接保存退出并重启整个程序即可。
 
-: 关闭控制台回显，不用动。
+: （非配置项）关闭控制台回显。
 @echo off
 
-: 服务端窗口标题栏显示的内容，不用动。
+: （非配置项）服务端窗口标题栏显示的内容。
 title DeepSeek To Go 服务端
 
-: 运行前前检查文件夹内是否有Ollama，不用动。
+: （非配置项）运行前前检查文件夹内是否有Ollama。
 if not exist ollama.exe (
     echo 没有在此文件夹内找到Ollama！请按[W]键下载zip包（“ollama-windows-amd64.zip”），并手动解压至此文件夹,再重启整个程序！
     choice /C:W /N
@@ -42,6 +43,9 @@ set OLLAMA_NUM_PARALLEL=4
 
 : Ollama可以同时加载的模型数量，如果要多开很多个客户端可以适量调大。
 set OLLAMA_MAX_LOADED_MODELS=4
+
+: （非配置项）此配置文件的版本，升级DeepSeek To Go时，若发现新版的配置文件比该文件版本高，你应该将旧的配置迁移到新版配置文件，并将旧版本替换。
+set config_version_client=1.0
 
 : Ollama服务端启动命令，可在此配置参数。
 ollama serve
